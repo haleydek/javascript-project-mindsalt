a18ad2313fb21ce73448f61c2383ecbbc79719f5
haleydek <hcd09@live.com>
2019-09-11 18:05:21 -0500

    render posts via addPostsToDOM function

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,31 @@
+let posts = document.getElementById("salt-index");
+
 function addPostsToDOM(postsCollection) {
+    const postsObjects = postsCollection["data"]
     const postsRelatedObj = postsCollection["included"]
-    console.log(postsRelatedObj)
+    // postsRelatedObj is array of user, category and hashtag objects
+    // that are related to the posts in postsCollection
+
+    for (postObj of postsObjects) {
+        posts.innerHTML += `
+            <div class="post-container">
+                <p class="post-content">${postObj.attributes.content}</p>
+                <p>${postObj.attributes.uplifts} Uplifts</p>
+                <button data-post-id=${postObj.id} class="uplift-btn">Uplift</button>
+            </div>
+        `
+    }
+
+    for (relatedObj of postsRelatedObj) {
+        // const hashtagCollection = []
+        // const userCollection = []
+        // const categoryCollection = []
+
+        // relatedObj.type === "category" ? 
+    }
 }
 
-fetch("https://localhost:3000/posts")
+fetch("http://localhost:3000/posts")
     .then(response => {
         return response.json();
     })
