da854234ff6c29574672a67d12ef0f0bad482c49
haleydek <hcd09@live.com>
2019-09-11 13:06:18 -0500

    rename categories column: type to name

--- mindsalt-api/db/schema.rb
+++ mindsalt-api/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_231832) do
+ActiveRecord::Schema.define(version: 2019_09_11_180413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
