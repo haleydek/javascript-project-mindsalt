c4e3f6bcfaa451ddb0bafc296c1215345a7ae059
haleydek <hcd09@live.com>
2019-09-23 16:42:14 -0500

    replaace submitButton event listener with newForm event listener. Add validateForm() to prevent blank fields from being sent to API

--- frontend/adapter.js
+++ frontend/adapter.js
@@ -13,7 +13,7 @@ class Adapter {
 
         this.postsContainer.addEventListener("click", this.filterOrUplift)
         this.navLinks.addEventListener("click", this.toggleActiveLink)
-        this.submitButton.addEventListener("click", this.handleSubmit)
+        this.newForm.addEventListener("submit", this.handleSubmit)
     }
 
     fetchPosts() {
@@ -98,27 +98,28 @@ class Adapter {
         let sortedPosts = this.sortPostsNewToOld(postsArray);
         
         sortedPosts.forEach(postObj => {
+            // use map/join combo
             this.postsContainer.appendChild(postObj.render(this.postsCategories, this.postsHashtags))
         })
     }
 
     toggleActiveLink = (e) => {
         if (e.target.className === "nav-link") {
-            this.removeActiveClass();
+            this.hideContent();
     
-            this.addActiveClass(e);
+            let targetId = e.target.getAttribute("href").substr(1);
+            let targetDiv = document.getElementById(`${targetId}`);
+            this.showContent(targetDiv);
         }
     }
     
-    removeActiveClass = () => {
+    hideContent = () => {
         let currentDiv = document.querySelector("div.active");
         currentDiv.classList.remove("active");
     }
-    
-    addActiveClass(e){
-        let targetId = e.target.getAttribute("href").substr(1);
-        let targetDiv = document.getElementById(`${targetId}`);
-        targetDiv.classList.add("active");
+
+    showContent = (target) => {
+        target.classList.add("active");
     }
 
     returnHome = () => {
@@ -140,45 +141,59 @@ class Adapter {
     handleSubmit = (e) => {
         e.preventDefault();
 
-        Array.from(this.formFieldsets).forEach(fieldset => {
-            let textarea = fieldset.getElementsByTagName("textarea")[0];
-            let hashtags = fieldset.getElementsByClassName("new-hashtag");
-            let hashtags_attributes = this.collectTags(hashtags);
-    
-            let newPostData = {
-                content: textarea.value,
-                category_id: fieldset.dataset.categoryId,
-                hashtags_attributes
-            }
-    
-            fetch("http://localhost:3000/posts", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                },
-                body: JSON.stringify(newPostData)
-            })
-            .then(res => res.json())
-            .then(dataObj => {
+        if (this.validateForm()){
 
-                dataObj["included"].forEach(obj => {
-                    if (obj.type === "hashtag" && !this.postsHashtags.find(tag => tag["id"] === obj.id)) {
-                        let newTag = new Hashtag(obj)
-                        this.postsHashtags.push(newTag)
-                    }
+            Array.from(this.formFieldsets).forEach(fieldset => {
+                let textarea = fieldset.getElementsByTagName("textarea")[0];
+                let hashtags = fieldset.getElementsByClassName("new-hashtag");
+                let hashtags_attributes = this.collectTags(hashtags);
+        
+                let newPostData = {
+                    content: textarea.value,
+                    category_id: fieldset.dataset.categoryId,
+                    hashtags_attributes
+                }
+        
+                fetch("http://localhost:3000/posts", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json"
+                    },
+                    body: JSON.stringify(newPostData)
                 })
-                
-                let newPost = new Post(dataObj["data"])
-                this.allPosts.push(newPost)
+                .then(res => res.json())
+                .then(dataObj => {
 
-                this.addPostsToDOM(this.allPosts)
+                    dataObj["included"].forEach(obj => {
+                        if (obj.type === "hashtag" && !this.postsHashtags.find(tag => tag["id"] === obj.id)) {
+                            let newTag = new Hashtag(obj)
+                            this.postsHashtags.push(newTag)
+                        }
+                    })
+                    
+                    let newPost = new Post(dataObj["data"])
+                    this.allPosts.push(newPost)
+
+                    this.addPostsToDOM(this.allPosts)
+                })
             })
-        })
 
-        this.removeActiveClass()
-        this.newForm.reset()
-        this.returnHome()
+            this.removeActiveClass()
+            this.newForm.reset()
+            this.returnHome()
+        } else {
+            alert("Response fields cannot be blank.")
+        }
+    }
+
+    validateForm() {
+        let contents = Array.from(document.querySelectorAll("textarea"));
+        let checkContents = contents.map(content => {
+            return (content.value === "") ? false : true
+        });
+
+        return checkContents.includes(false) ? false : true
     }
 }
 
